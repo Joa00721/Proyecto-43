29f3a413bd7a7cf0879c117d96623f12e78ff4e1
Joa00721 <86857876+Joa00721@users.noreply.github.com>
2021-10-21 18:22:21 -0500

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("red")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("gold")
+    text("Hint: Is always changing, its never constant", 100,70)
+
+    fill("red")
+    textSize(15)
+    text("C U N I F O N T", 700,150)
+    fill("gold")
+    text("Hint: It makes a specific task", 700,170)
+
+    fill("red")
+    textSize(15)
+    text("E D A T E D O S B A S", 100,250)
+    fill("gold")
+    text("Hint: It saves all the information", 100,270)
+}
